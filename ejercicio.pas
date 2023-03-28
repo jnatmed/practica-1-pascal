84c2846d7948773582997b089a66e3d55dcec68c
jnatmed <49349742+jnatmed@users.noreply.github.com>
2023-03-28 15:17:59 -0300

    terminando juego. Falta refrescar pantalla

--- ejercicio.pas
+++ ejercicio.pas
@@ -1,6 +1,8 @@
 
 Program mijuego;
 
+Uses WinCrt, Crt;
+
 Var verticalIndex,horizontalIndex, xPlayer, yPlayer : Integer;
 
 Var tecla : Char;
@@ -22,7 +24,17 @@ Begin
           WriteLn('');
         End;
       writeLn('Press Key');
-      ReadLn(tecla);
-      writeln(tecla);
+      tecla := Readkey;
+      If (tecla = 'w') And (xPlayer > 1) Then
+        xPlayer := xPlayer -1;
+      If (tecla = 's') And (xPlayer < 8) Then
+        xPlayer := xPlayer +1;
+      If (tecla = 'a') And (yPlayer > 1) Then
+        yPlayer := yPlayer -1;
+      If (tecla = 'd') And (yPlayer < 14) Then
+        yPlayer := yPlayer +1;
+      WriteLn(tecla);
+      ClrScr;
+      // WriteLn(yPlayer);
     End;
 End.
